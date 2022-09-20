38d9b41125eba82cafab03b6957515f0e37fd81a
plomero <plomero@gmail.com>
2022-09-19 21:24:11 -0500

    cambie barra de navegacion

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Salas de juntas</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <router-link class="navbar-brand" to="/">Reservaciones</router-link>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link class="nav-link" to="/">Salas de juntas</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/book-meeting-room-list">Salas reservadas</router-link>
+        </li>
+
+      </ul>
+    </div>
+
   </nav>
   <div class="container">
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
